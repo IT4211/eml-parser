b5c351c8d069bd4a31b61a52e698b7a40b0b5fca
IT4211 <rhdqor100@live.co.kr>
2017-01-24 04:30:16 +0900

    Update whois_search.py
    whois 검색과 결과 출력 구현 완료

--- eml_parser.py
+++ eml_parser.py
@@ -21,14 +21,42 @@ class parser_window(QtGui.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.actionOpen.triggered.connect(self.browse_folder)
+        self.ui.IPSearch_Button.clicked.connect(self.ip_search)
+        self.ui.parsing_pushButton.clicked.connect(self.eml_parsing)
 
     def browse_folder(self):
         self.directory = QtGui.QFileDialog.getExistingDirectory(self, "Select Directory")
+        self.ui.pathdisplay_lineEdit.setText(self.directory)
 
     def ip_search(self):
         ip_addr = self.ui.IPinput_lineEdit.text()
         # 실제 정확한 ip address를 입력했는지 검증할 것! 정규표현식 등 ..
-        whois_search.get_ip_info(ip_addr)
+        result = whois_search.get_ip_info(str(ip_addr))
+        self.ui.Whois_res_TextEdit.setText(result)
+
+    def eml_parsing(self):
+        if self.directory:
+            emlDir = str(self.directory) + "\\"
+            # multi processing !!
+            corePool = multiprocessing.Pool(processes=4)
+            # GUI를 통해서 폴더 명이 들어오면, 그 폴더명은 emldir 이라는 변수에 저장
+            # 그리고 그 폴더 내에 있는 목록을 얻어 와야한다.
+            listOfFiles = glob.glob("*.eml")
+            # 파일이 정말 많을 때
+            # for i in glob.iglob("*.eml"):
+            # 4개를 돌아가면서 넣어주기?
+            if len(listOfFiles) > 3:
+                for i in range(0, len(listOfFiles), 4):  # 이렇게 해도 되나?
+                    corePool.map(eml_file_handling, (emlDir + listOfFiles[i],
+                                                     emlDir + listOfFiles[i + 1],
+                                                     emlDir + listOfFiles[i + 2],
+                                                     emlDir + listOfFiles[i + 3]))
+
+            elif len(listOfFiles) == 1:
+                # Test case
+                eml_file_handling(emlDir + listOfFiles[0])
+            else:
+                print "[err] Too few files.", len(listOfFiles), listOfFiles
 
 
 if __name__=="__main__":
@@ -37,28 +65,4 @@ if __name__=="__main__":
     eml_parser.show()
     app.exec_()
 
-
-    emlDir = str(eml_parser.directory) + "\\"
-    print "[debug:emlDir]", emlDir
-    # multi processing !!
-    corePool = multiprocessing.Pool(processes=4)
-
-    # GUI를 통해서 폴더 명이 들어오면, 그 폴더명은 emldir 이라는 변수에 저장
-    # 그리고 그 폴더 내에 있는 목록을 얻어 와야한다.
-    listOfFiles = glob.glob("*.eml")
-    # 파일이 정말 많을 때
-    #for i in glob.iglob("*.eml"):
-        # 4개를 돌아가면서 넣어주기?
-    if len(listOfFiles) > 3:
-        for i in range(0, len(listOfFiles), 4): # 이렇게 해도 되나?
-            corePool.map(eml_file_handling, (emlDir + listOfFiles[i],
-                                             emlDir + listOfFiles[i+1],
-                                             emlDir + listOfFiles[i+2],
-                                             emlDir + listOfFiles[i+3]))
-
-    elif len(listOfFiles) == 1:
-        # Test case
-        eml_file_handling(emlDir + listOfFiles[0])
-    else:
-        print "[err] Too few files.", len(listOfFiles), listOfFiles
 
--- whois_search.py
+++ whois_search.py
@@ -1,20 +1,5 @@
 #-*- coding: utf-8 -*-
-"""
-whois 검색 결과 : json 형태
-파싱할 내용 및 json 항목
-['whois']['contryCode']
-['whois']['korean']['user']['netinfo']['orgName']
-['whois']['korean']['user']['netinfo']['orgID']
-['whois']['korean']['user']['netinfo']['addr']
-['whois']['korean']['user']['netinfo']['zipCode']
-['whois']['korean']['user']['netinfo']['regDate']
-['whois']['korean']['user']['techContact']['name']
-['whois']['korean']['user']['techContact']['phone']
-['whois']['korean']['user']['techContact']['email']
-json 정렬 : http://json.parser.online.fr/
-"""
 
-import struct
 import urllib
 import json
 
@@ -27,25 +12,41 @@ def get_ip_info(ip_addr):
                    "query="+ ip_addr +"&" \
                    "key=2017010901372246522659&" \
                    "answer=json"
-    ip_info = urllib.urlopen(whois_ip_url)
-    print ip_info.read()
+    result = urllib.urlopen(whois_ip_url)
+    ip_info = result.read()
+    result.close()
+    return ip_info_parsing(ip_info)
 
 def ip_info_parsing(ip_info):
+    print "[debug]", ip_info
     json_info = json.loads(ip_info)
 
-    contryCode = json_info['whois']['contryCode']
-    orgName = json_info['whois']['korean']['user']['netinfo']['orgName']
-    addr = json_info['whois']['korean']['user']['netinfo']['addr']
-    zipCode = json_info['whois']['korean']['user']['netinfo']['zipCode']
-    regDate = json_info['whois']['korean']['user']['netinfo']['regDate']
-    name = json_info['whois']['korean']['user']['techContact']['name']
-    phone = json_info['whois']['korean']['user']['techContact']['phone']
-    email = json_info['whois']['korean']['user']['techContact']['email']
+    if 'korean' in json_info['whois']:
+        countryCode = json_info['whois']['countryCode']
+        orgName = json_info['whois']['korean']['user']['netinfo']['orgName']
+        addr = json_info['whois']['korean']['user']['netinfo']['addr']
+        zipCode = json_info['whois']['korean']['user']['netinfo']['zipCode']
+        regDate = json_info['whois']['korean']['user']['netinfo']['regDate']
+        name = json_info['whois']['korean']['user']['techContact']['name']
+        phone = json_info['whois']['korean']['user']['techContact']['phone']
+        email = json_info['whois']['korean']['user']['techContact']['email']
+
+        res = "CountryCode : " + countryCode + "\n"
+        res += "OrgName : " + orgName + "\n"
+        res += "Address : " + addr + "\n"
+        res += "ZipCode : " + zipCode + "\n"
+        res += "RegDate : " + regDate + "\n"
+        res += "Name : " + name + "\n"
+        res += "Phone : " + phone + "\n"
+        res += "Email : " + email + "\n"
+    else:
+        countryCode = json_info['whois']['countryCode']
+        registry = json_info['whois']['registry']
+
+        res = "CountryCode : " + countryCode + "\n"
+        res += "Registry : " + registry + "\n"
 
 
 
-
-
-
-
+    return res
 
